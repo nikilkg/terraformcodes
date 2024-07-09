e9cd1201c8e89f7ea652d9a2e414fca391d2df66
nikilkg <37948754+nikilkg@users.noreply.github.com>
2024-07-09 20:27:02 +0530

    Update aws_key.tf

--- aws_key.tf
+++ aws_key.tf
@@ -1,6 +1,19 @@
-# creating ssh-key
-resource "aws_key_pair" "tf-key" {
-  key_name = "ec2-key"
-# public_key = "file(${path.module}/ec2-key.pub)"
-  public_key = "${file("ec2-key.pub")}"
+resource "aws_key_pair" "tf_key" {
+    key_name   = "tf-key"
+    public_key = tls_private_key.rsa.public_key_openssh
+    tags = {
+        managed_by = "terraform-managed"
+        }
+}
+
+# RSA key of size 4096 bits
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# create local file to store private key
+resource "local_file" "tf-key" {
+  content = tls_private_key.rsa.private_key_pem
+  filename = "tfkey"
 }
