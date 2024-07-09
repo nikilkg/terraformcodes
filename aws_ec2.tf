a1290d810b74ac649eb8fe83275b330e18fa7702
nikilkg <37948754+nikilkg@users.noreply.github.com>
2024-07-09 20:26:23 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -1,12 +1,11 @@
-resource "aws_instance" "ec2-free" {
+resource "aws_instance" "ec2-01" {
     tags = {
       name = "server1"
-      department = "finance"
+      managed_by = "terraform-managed"
     }
     ami = "ami-01376101673c89611"
     instance_type = "t2.micro"
-    subnet_id = "subnet-0cee7a5a0949da16d"
-    key_name = "ec2-key"
-    vpc_security_group_ids = ["sg-075eda11043191529", "sg-0201cd65b37a659e3"]
+    subnet_id = aws_subnet.tf-subnet-pub-01.id
+    key_name = "tf-key"
     count = 1
 }
