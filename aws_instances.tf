3df7623820fe1e328d01a7f9a76ad102740182ba
nikilkg <37948754+nikilkg@users.noreply.github.com>
2024-07-11 01:03:25 +0530

    Update aws_instances.tf

--- aws_instances.tf
+++ aws_instances.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "ec2-01" {
     ami = "ami-01376101673c89611"
     instance_type = "t2.micro"
     subnet_id = aws_subnet.tf-subnet-pub-01.id
-    key_name = "tf-key"
+    key_name = "tf-key-01"
     count = 1
 }
